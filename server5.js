b4b1bec0240d7ba35ee960cf0322e28848de396c
C07-T\강사 <knyatom@daum.net>
2023-06-14 16:55:39 +0900

    welcome

--- server5.js
+++ server5.js
@@ -9,8 +9,34 @@ app.use(app.router);
 
 app.post('/user', function(request, response) {
     var name = request.param('user_name');
+    //var name =request.query.user_name;
     response.send(name);
 });
+
+app.get("/parameter",function(request,response){
+ var name=request.query.name;
+ var region=request.query.region;
+
+ response.send(`<h1>${name} : ${region}</h1>`);
+});
+
+var items=[
+ {name:'우유', price:200},
+ {name:'홍차', price:300},
+ {name:'커피', price:500} 
+];
+
+app.all('/data.html',function(req,response){
+  var out="";
+  items.forEach(function(item){
+   out+="<div>";
+   out+="<span>"+item.name+ ":" + item.price+"원</span>";
+   out+="</div>";
+  });
+  response.send(out);
+});
+
+
 
 // 웹 서버를 실행합니다.
 http.createServer(app).listen(52275, function() {
