3f2a52324cac2575040fb997a41853842792f537
C07-T\강사 <knyatom@daum.net>
2023-06-21 12:36:56 +0900

    전체회원보기

--- student.js
+++ student.js
@@ -22,40 +22,50 @@ app.use(app.router);
 // 라우터 설정
 // 전체조회
 app.get('/students', function (req, res) {
+var sql="select * from students";
+client.query(sql,function(err,result){
+ res.send(result);
+});
 
 });  // get end
 
 // 개별조회
 app.get('/students/:id', function (req, res) {
- 
+
 }); // 개별조회 .. 
 
 // 데이터추가
 app.post('/students', function (req, res) {
-var id=req.body.id;
-var name=req.body.name;
-var pw=req.body.password;
-var email=req.body.email;
-var gender=req.body.gender;
-var data={ id, name, pw, email, gender};
-console.log(data);
+ var id = req.body.id;
+ var name = req.body.name;
+ var pw = req.body.password;
+ var email = req.body.email;
+ var gender = req.body.gender;
+ var data = { id, name, pw, email, gender };
+ console.log(data);
 
-// DB연동
-var query="insert into students (id,name,password,email,gender) values (?,?,?,?,?)";
+ // DB연동
+ var sql = "insert into students (id,name,password,email,gender) values (?,?,?,?,?)";
+ //var query = "insert into students (id,name,pw,email,gender) values ('" + id + "','" + name + "','" + pw + "','" + email + "', '" + gender + "')";
+ client.query(sql, [id, name, pw, email, gender], function (err, result) {
+  if (err) console.log(err);
 
+  //res.send(result);
+  res.redirect("member_ok.html");
 
+ });
 
 
 }); // post end
 
 // 데이터 수정
 app.put('/students/:id', function (req, res) {
- 
+
 });
 
 // 데이터 삭제
 app.del('/students/:id', function (req, res) {
- 
+
 });
 
 
